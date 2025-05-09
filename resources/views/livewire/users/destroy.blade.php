3ec260edd46dafb9bf83aa4b6e62e1391a57541a
Jonne C C Silva <jonne@zamed.com.br>
2025-05-09 18:25:59 -0300

    chore: adicionada a UI do componente de delete

--- resources/views/livewire/users/destroy.blade.php
+++ resources/views/livewire/users/destroy.blade.php
@@ -1,3 +1,10 @@
-<div>
-    <h1>DESTROY</h1>
-</div>
+<x-zmd-modal title="Aviso!" wire persistent center>
+    <p>O usuário <span class="font-bold">{{ $name }}</span> será deletado. </p>
+    <p class="text-sm text-gray-500">Essa operação é irreversível!</p>
+
+
+    <x-slot:footer>
+    <x-zmd-button type="submit" text="Cancelar" color="secondary" sm wire:click="$set('modal', false)"/>
+    <x-zmd-button type="button" text="Deletar" color="rose" sm wire:click='handle' loading/>
+    </x-slot:footer>
+</x-zmd-modal>
