d504b19cd919906b1527c61e8cc98ee44af72153
Jonne C C Silva <jonne@zamed.com.br>
2025-05-09 18:28:44 -0300

    refactor: adicionada a lógica para não apagar a si mesmo

--- resources/views/livewire/users/index.blade.php
+++ resources/views/livewire/users/index.blade.php
@@ -1,6 +1,8 @@
     <x-zmd-table :headers="$this->headers" :rows="$this->rows" filter paginate id="users">
         @interact('column_action', $row) 
-            <x-zmd-button.circle color="gray" flat sm icon="pencil"
-                wire:click="$dispatch('users::edit', {id: '{{ $row->id }}'})" />
+            <x-zmd-button.circle color="gray" flat sm icon="pencil" wire:click="$dispatch('users::edit', {id: '{{ $row->id }}'})" />
+            @if (Auth::id() !== $row->id)
+                <x-zmd-button.circle color="red" flat sm icon="trash" wire:click="$dispatch('users::destroy', {id: '{{ $row->id }}'})" />
+            @endif
         @endinteract
     </x-zmd-table>
