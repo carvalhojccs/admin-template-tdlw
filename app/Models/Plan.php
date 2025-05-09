b20f373a198e1b443d25d8a8ab5de74483334d29
Jonne C C Silva <jonne@zamed.com.br>
2025-05-09 18:19:53 -0300

    chore: criado a model do plano

---
+++ app/Models/Plan.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use App\Observers\PlanObserver;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Attributes\ObservedBy;
+use Illuminate\Support\Number;
+
+#[ObservedBy(PlanObserver::class)]
+class Plan extends Model
+{
+    protected $guarded = ['id'];
+
+    protected $casts = [
+        'price' => 'decimal:2',
+    ];
+
+protected function price(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => Number::currency($value, 'BRL', 'pt_br'),
+            //set: fn ($value) => Number::currency($value, 'US'),
+        );
+    }
+
+    public function users()
+    {
+        return $this->hasMany(User::class);
+    }
+
+}
