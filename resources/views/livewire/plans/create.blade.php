b62a1d1d35327a4c6e1fa26d9f13a4a2672d9118
Jonne C C Silva <jonne@zamed.com.br>
2025-05-15 18:41:53 -0300

    chore: implementando o show page component

--- resources/views/livewire/plans/create.blade.php
+++ resources/views/livewire/plans/create.blade.php
@@ -1,15 +1,34 @@
-<div>
-    <x-zmd-modal title="Cadastrar novo plano" wire persistent center>
-        <form wire:submit='handle' id="create-plan-form">
-            <x-zmd-input label="Nome *" hint="Insira o nome do plano" wire:model='name' />
-            <x-zmd-input label="Preço *" hint="Insira o valor do plano" wire:model='price' type="text" />
-            
-            <x-zmd-textarea label="Descrição" hint="Insira uma descrição para o plano" wire:model='description' maxlength="200" count />
+<x-zmd-modal title="Cadastrar novo plano" wire persistent center  x-on:open="$focusOn('name')">
+    <form wire:submit='handle' id="create-plan-form">
+        <x-zmd-input
+            label="Nome *"
+            hint="Insira o nome do plano"
+            wire:model='name'
+            x-ref="name"
+            x-on:keydown.enter.prevent="$refs.price.focus()"
+        />
 
-        </form>
-        <x-slot:footer>
-        <x-zmd-button type="submit" text="Cancelar" color="secondary" sm wire:click="$set('modal', false)"/>
-        <x-zmd-button form="create-plan-form" type="submit" text="Salvar" color="orange" sm/>
-        </x-slot:footer>
-    </x-zmd-modal>
-</div>
+        <x-zmd-input
+            label="Preço *"
+            hint="Insira o valor do plano"
+            wire:model='price'
+            type="text"
+            x-mask:dynamic="$money($input, ',')"
+            x-ref="price"
+            x-on:keydown.enter.prevent="$refs.textarea.focus()"
+        />
+
+        <x-zmd-textarea
+            label="Descrição"
+            hint="Insira uma descrição para o plano"
+            wire:model='description'
+            maxlength="200"
+            count
+        />
+
+    </form>
+    <x-slot:footer>
+    <x-zmd-button type="submit" text="Cancelar" color="secondary" sm wire:click="close"/>
+    <x-zmd-button form="create-plan-form" type="submit" text="Salvar" color="orange" sm/>
+    </x-slot:footer>
+</x-zmd-modal>
--- routes/plan.php
+++ routes/plan.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Livewire\Plans\Show;
 use App\Livewire\Plans\Plans;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth'])->group(function (): void {
     Route::get('/plans', Plans::class)->name('plans.plans');
+    Route::get('/plans/show/{plan}', Show::class)->name('plans.show');
 });
